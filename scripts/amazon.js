7affa857e2051b2e416d5a4dc0d1c8236df64e81
logi4ace-coder <u24742083@tuks.co.za>
2025-06-24 19:43:08 +0200

    Add cart count.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -127,12 +127,29 @@ function addProductToCart(element, productId){
         localStorage.setItem('cart', JSON.stringify(parsedgetLSCart));
 
     }
+    quanityCount();
+
 }
 function uniqueID() {
     return crypto.randomUUID();
   }
   
+  function quanityCount(){
+    let getLSCart = (localStorage.getItem('cart'));// array of objects
 
+    let quanityCount = document.querySelector('.cart-quantity');
+
+    if(getLSCart==null || getLSCart=='null' || JSON.parse(getLSCart).length==0){
+      quanityCount.textContent=0;
+
+    }   
+    else{
+        let parsedgetLSCart = JSON.parse(getLSCart);
+        quanityCount.textContent= parsedgetLSCart.length;
+
+    }
+  }
+quanityCount();
 /*
 1
 : 
